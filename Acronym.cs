94ccc3b0fc94d48de7c8b444bb677ac79911b34c
Sara Radojević <sarkica97@gmail.com>
2020-12-05 17:42:39 +0100

    Add project files.

---
+++ Acronym.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        char[] delimiterChars = { ' ', '-', '_' };
+        string[] words = phrase.Split(delimiterChars, );
+        string abbr = "";
+
+        foreach (string word in words)
+        {            
+            abbr += word[0];
+        }
+
+        return abbr.ToUpper();
+
+    }
+}
